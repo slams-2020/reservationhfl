b82acd233e49773035ed630fd2552eab40e406d7
Freaky5 <56235179+Freaky5@users.noreply.github.com>
2020-10-15 12:02:38 +0200

    Update MainController.java

--- src/main/java/net/reserv/controllers/MainController.java
+++ src/main/java/net/reserv/controllers/MainController.java
@@ -2,15 +2,19 @@ package net.reserv.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
-import io.github.jeemv.springboot.vuejs.VueJS;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
 	@Autowired
-	private VueJS vue;
+	// private VueJS vue;
 
-	private String URL = "http://127.0.0.1:8080/";
+	// private String restURL = "http://127.0.0.1:8080/";
 
+	@RequestMapping("/")
+	public String index() {
+
+		return "index";
+	}
 }
