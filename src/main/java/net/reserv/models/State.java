6d0179b3d2f3c0203b248a0096021748e9bea21d
Louis Anfrye <lanfrye@sio.local>
2020-10-15 10:26:13 +0200

    Update State.java

--- src/main/java/net/reserv/models/State.java
+++ src/main/java/net/reserv/models/State.java
@@ -1,12 +1,21 @@
 package net.reserv.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+@Entity
 public class State {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id_state;
 	private String statename;
+	
+	@ManyToOne
+	private City city;
 
 	public int getId_state() {
 		return id_state;
