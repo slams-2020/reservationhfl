c05ef2d8dab429d9e49492ff4f7298027f2b364e
Louis Anfrye <lanfrye@sio.local>
2020-10-15 11:56:52 +0200

    Update LocationRepository.java

--- src/main/java/net/reserv/repository/java/LocationRepository.java
+++ src/main/java/net/reserv/repository/java/LocationRepository.java
@@ -6,7 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.reserv.models.Location;
 
+
 public interface LocationRepository extends JpaRepository<Location, Integer> {
 
+	List<Location> findByid_name(int id_location);
+	List<Location> findByemail(String gps);
+	List<Location> findBypassword(String address1);
+	List<Location> findByfistname(String address2);
 	
 }
