f4df922ed88def3ee7940a33839776656de9d262
Louis Anfrye <lanfrye@sio.local>
2020-10-15 11:46:58 +0200

    Create CaracteristicRepository.java

---
+++ src/main/java/net/reserv/repository/java/CaracteristicRepository.java
@@ -0,0 +1,12 @@
+package net.reserv.repository.java;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.reserv.models.Caracteristic;
+
+public interface CaracteristicRepository extends JpaRepository<Caracteristic, Integer> {
+
+	
+}
