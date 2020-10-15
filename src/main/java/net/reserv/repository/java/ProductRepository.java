3695f05e331601ffd3edc87b0182dd83b2b23dca
Louis Anfrye <lanfrye@sio.local>
2020-10-15 11:56:55 +0200

    Update ProductRepository.java

--- src/main/java/net/reserv/repository/java/ProductRepository.java
+++ src/main/java/net/reserv/repository/java/ProductRepository.java
@@ -6,7 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.reserv.models.Product;
 
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 	
+	List<Product> findByid_name(int id_product);
+	List<Product> findByemail(String type);
+	List<Product> findBypassword(String title);
+	List<Product> findByfistname(String description);
+	List<Product> findBylastname(String price);
 }
