a9d53e46d49131155406ed4eba6c0c499f71d654
lanfrye <62891205+lanfrye@users.noreply.github.com>
2020-11-02 13:30:29 +0100

    Update ProductController.java

--- src/main/java/net/reserv/controllers/ProductController.java
+++ src/main/java/net/reserv/controllers/ProductController.java
@@ -1,5 +1,26 @@
 package net.reserv.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import net.reserv.models.Product;
+import net.reserv.repository.java.ProductRepository;
+
+@Controller
 public class ProductController {
+
+		@Autowired
+		
+		private ProductRepository repo;
+		
+		@PostMapping("new")
+		@ResponseBody
+		public String newProduct(Product products) {
+			repo.saveAndFlush(products);
+			return products+"ajoutée.";
+	}
 
 }
