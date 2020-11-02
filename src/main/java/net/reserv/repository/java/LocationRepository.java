9b4c3ae178eb56f2bb83c61c3612cb25b29b8450
lanfrye <62891205+lanfrye@users.noreply.github.com>
2020-11-02 14:40:34 +0100

    Update LocationRepository.java

--- src/main/java/net/reserv/repository/java/LocationRepository.java
+++ src/main/java/net/reserv/repository/java/LocationRepository.java
@@ -3,9 +3,10 @@ package net.reserv.repository.java;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import net.reserv.models.Location;
-
+@EnableJpaRepositories
 
 public interface LocationRepository extends JpaRepository<Location, Integer> {
 
