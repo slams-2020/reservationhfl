1d7d44488c7a3dc234ca25d0392d3d8ac199b5d5
lanfrye <62891205+lanfrye@users.noreply.github.com>
2020-11-02 14:40:31 +0100

    Update ProductRepository.java

--- src/main/java/net/reserv/repository/java/ProductRepository.java
+++ src/main/java/net/reserv/repository/java/ProductRepository.java
@@ -3,10 +3,10 @@ package net.reserv.repository.java;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import net.reserv.models.Product;
-
-
+@EnableJpaRepositories
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 	
