35634d5c2364388e05776797bd9ba51d06c69174
lanfrye <62891205+lanfrye@users.noreply.github.com>
2020-11-02 14:40:36 +0100

    Update CaracteristicRepository.java

--- src/main/java/net/reserv/repository/java/CaracteristicRepository.java
+++ src/main/java/net/reserv/repository/java/CaracteristicRepository.java
@@ -3,10 +3,11 @@ package net.reserv.repository.java;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import net.reserv.models.Caracteristic;
 
-
+@EnableJpaRepositories
 public interface CaracteristicRepository extends JpaRepository<Caracteristic, Integer> {
 
 	List<Caracteristic> findByid_carateritic(int id_caracteristic);
