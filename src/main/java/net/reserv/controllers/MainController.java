9dce9ae89c21b918d936a9f82d6a71fdfba04a1b
lanfrye <62891205+lanfrye@users.noreply.github.com>
2020-11-02 14:40:38 +0100

    Update MainController.java

--- src/main/java/net/reserv/controllers/MainController.java
+++ src/main/java/net/reserv/controllers/MainController.java
@@ -4,13 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.github.jeemv.springboot.vuejs.VueJS;
+
 @Controller
 public class MainController {
 
 	@Autowired
-	// private VueJS vue;
+	 private VueJS vue;
 
-	// private String restURL = "http://127.0.0.1:8080/";
+	private String restURL = "http://127.0.0.1:8080/";
 
 	@RequestMapping("/")
 	public String index() {
