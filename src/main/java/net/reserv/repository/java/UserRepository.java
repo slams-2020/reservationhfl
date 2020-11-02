c5056cdbf5e9c6102b1ccf1c4a9e79e08132058f
lanfrye <62891205+lanfrye@users.noreply.github.com>
2020-11-02 14:40:26 +0100

    Update UserRepository.java

--- src/main/java/net/reserv/repository/java/UserRepository.java
+++ src/main/java/net/reserv/repository/java/UserRepository.java
@@ -3,9 +3,10 @@ package net.reserv.repository.java;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import net.reserv.models.User;
-
+@EnableJpaRepositories
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 	List<User> findByid_name(int id_user);
