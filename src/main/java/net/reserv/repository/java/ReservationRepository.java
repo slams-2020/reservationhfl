97c3c40af5d847b22dcd369f96b88a7c9b0e5a14
lanfrye <62891205+lanfrye@users.noreply.github.com>
2020-11-02 14:40:28 +0100

    Update ReservationRepository.java

--- src/main/java/net/reserv/repository/java/ReservationRepository.java
+++ src/main/java/net/reserv/repository/java/ReservationRepository.java
@@ -2,10 +2,13 @@ package net.reserv.repository.java;
 
 import java.util.List;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import net.reserv.models.Reservation;
 
+@EnableJpaRepositories
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
 	
 	List<Reservation> findByid_reservation(int id_reservation);
