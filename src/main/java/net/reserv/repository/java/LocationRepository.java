705c7b5542de642690631d24be8929d9582ebc1d
Freaky5 <56235179+Freaky5@users.noreply.github.com>
2020-11-03 16:24:46 +0100

    Update LocationRepository.java

--- src/main/java/net/reserv/repository/java/LocationRepository.java
+++ src/main/java/net/reserv/repository/java/LocationRepository.java
@@ -3,22 +3,20 @@ package net.reserv.repository.java;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-
-import net.reserv.models.Location;
-
-=======
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import net.reserv.models.Location;
+
 @EnableJpaRepositories
->>>>>>> 9dce9ae89c21b918d936a9f82d6a71fdfba04a1b
 
 public interface LocationRepository extends JpaRepository<Location, Integer> {
 
 	List<Location> findByid_name(int id_location);
+
 	List<Location> findByemail(String gps);
+
 	List<Location> findBypassword(String address1);
+
 	List<Location> findByfistname(String address2);
-	
+
 }
