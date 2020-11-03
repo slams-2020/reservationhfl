dd8c76897b38ff899d183cd268bb8dee4ebb3391
Freaky5 <56235179+Freaky5@users.noreply.github.com>
2020-11-03 16:24:41 +0100

    Update ReservationRepository.java

--- src/main/java/net/reserv/repository/java/ReservationRepository.java
+++ src/main/java/net/reserv/repository/java/ReservationRepository.java
@@ -2,25 +2,20 @@ package net.reserv.repository.java;
 
 import java.util.List;
 
-<<<<<<< HEAD
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.reserv.models.Reservation;
-
-=======
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import net.reserv.models.Reservation;
 
 @EnableJpaRepositories
->>>>>>> 9dce9ae89c21b918d936a9f82d6a71fdfba04a1b
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
-	
+
 	List<Reservation> findByid_reservation(int id_reservation);
+
 	List<Reservation> findByprice(String price);
+
 	List<Reservation> findBystartdate(String startDate);
+
 	List<Reservation> findByendDate(String endDate);
-	
+
 }
