060571c296a0251d4b11cb826de02e12fff033bb
Felisky <62891363+Felisky@users.noreply.github.com>
2020-11-03 16:01:00 +0100

    Create MainController.java

---
+++ src/main/java/net/reserv/controllers/MainController.java
@@ -0,0 +1,23 @@
+package net.reserv.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.github.jeemv.springboot.vuejs.VueJS;
+
+@Controller
+public class MainController {
+	
+	@Autowired
+	 private VueJS vue;
+
+	 private String restURL = "http://127.0.0.1:8080/";
+
+	@RequestMapping("/index.html")
+	public String index() {
+
+		return "index";
+	}
+	
+}
