ae737a4591af71c241f26a6a64af5f9f570c07fc
Felisky <62891363+Felisky@users.noreply.github.com>
2020-11-05 08:37:04 +0100

    Update ProductRepository.java

--- src/main/java/net/reserv/repository/java/ProductRepository.java
+++ src/main/java/net/reserv/repository/java/ProductRepository.java
@@ -7,17 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import net.reserv.models.Product;
 
-@EnableJpaRepositories
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-	List<Product> findByid_name(int id_product);
-
-	List<Product> findByemail(String type);
-
-	List<Product> findBypassword(String title);
-
-	List<Product> findByfistname(String description);
-
-	List<Product> findBylastname(String price);
 
 }
